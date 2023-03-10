c06e2611af54f65d34bafe748554a48cd5b170cb
Svetoslav Ivanov <svivanov@softserveinc.com>
2023-03-10 13:14:47 +0200

    Add dedicated routes to handle the different incoming request

---
+++ cmd/web/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gitlab.com/code-harbor/viwallet/internal/handlers"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(CSRFProtect)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Home)
+
+	mux.Get("/user/login", handlers.GetLogin)
+	mux.Post("/user/login", handlers.PostLogin)
+
+	mux.Get("/user/register", handlers.GetRegister)
+	mux.Post("/user/register", handlers.PostRegister)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
