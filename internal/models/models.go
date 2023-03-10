4dd10ea24d7e2c775c14956ac7b0a565443bdcef
Svetoslav Ivanov <svivanov@softserveinc.com>
2023-03-10 13:20:01 +0200

    Add models structure for the cards and the users

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,11 +3,17 @@ package models
 import "time"
 
 type User struct {
-	ID        int
-	Email     string
-	Password  string
-	FirstName string
-	LastName  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID          int
+	Email       string
+	Password    string
+	PhoneNumber string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Card struct {
+	CardNumber     int
+	ExpirationDate time.Time
+	CVCNumber      int
+	NamesOnCard    string
 }
