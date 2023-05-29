f175c6e7bda59781230fe02ccb5f17ff87453c4d
Svetoslav Ivanov <svetoslav.ivanov@windowslive.com>
2023-05-29 15:37:15 +0300

    Add sesseion configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/gob"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
+
+	"gitlab.com/code-harbor/viwallet/internal/config"
 	"gitlab.com/code-harbor/viwallet/internal/driver"
 	"gitlab.com/code-harbor/viwallet/internal/handlers"
-	"gitlab.com/code-harbor/viwallet/internal/repository/dbrepo"
+	"gitlab.com/code-harbor/viwallet/internal/models"
+	"gitlab.com/code-harbor/viwallet/internal/render"
 )
 
-const dbHost string = "localhost"
-const dbPort string = "5432"
-const dbName string = "viwallet"
-const dbUser string = "postgres"
-const dbPassword = "LQIHr6NEvDrY@1cW0hOe1WBEA2G$&2sX"
-const dsnStr = "host=%s port=%s dbname=%s user=%s password=%s"
+var session *scs.SessionManager
+var app config.AppConfig
+var infoLog *log.Logger
+var errorLog *log.Logger
+
+const DbHost string = "localhost"
+const DbPort string = "5432"
+const DbName string = "viwallet"
+const DbUser string = "postgres"
+const DbPassword = "LQIHr6NEvDrY@1cW0hOe1WBEA2G$&2sX"
+const DSNStr = "host=%s port=%s dbname=%s user=%s password=%s"
 
 func main() {
-
 	conn, err := run()
+
 	if err != nil {
 		// log server error
-		conn.Close()
-		log.Fatal(err)
+		app.ErrorLog.Fatal(err)
 	}
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer conn.SQL.Close()
+
 	// get the enviroment type (prod, stg, dev)
 	runtimeSetup := os.Getenv("RUNTIME_SETUP")
 	if runtimeSetup == "" {
@@ -57,8 +65,8 @@ func main() {
 		serverPort = ":" + serverPort
 	}
 
-	log.Println("Application running in environment:", runtimeSetup)
-	log.Printf("Starting the application, listening on port %s", serverPort)
+	app.InfoLog.Println("Application running in environment:", runtimeSetup)
+	app.InfoLog.Printf("Starting the application, listening on port %s", serverPort)
 
 	srv := &http.Server{
 		Addr:    serverPort,
@@ -67,21 +75,57 @@ func main() {
 
 	err = srv.ListenAndServe()
 
-	log.Fatal(err)
+	app.InfoLog.Fatal(err)
 }
 
-func run() (*sql.DB, error) {
-	var dsn string = fmt.Sprintf(dsnStr, dbHost, dbPort, dbName, dbUser, dbPassword)
+func run() (*driver.DB, error) {
+	app.InProduction = false
+	app.UseCache = app.InProduction
+
+	// Configure
+	gob.Register(models.User{})
+
+	// configure session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	var dsn string = fmt.Sprintf(DSNStr, DbHost, DbPort, DbName, DbUser, DbPassword)
 
 	// init db connection
 	dbConn, err := driver.ConnectSQLDatabase(dsn)
 	if err != nil {
-		dbConn.Close()
-		log.Fatal("Server error. Error establishin connection to the database.")
+		dbConn.SQL.Close()
+		app.ErrorLog.Fatal("Server error. Error establishin connection to the database.")
 	}
 
-	repo := dbrepo.NewPostgresRepo(dbConn)
-	handlers.SetDBRepo(repo)
+	app.InfoLog.Println("Connected to database.")
+
+	if app.UseCache {
+
+		// create template cache
+		tc, err := render.CreateTemplateCache()
+		if err != nil {
+			app.ErrorLog.Fatal("cannot create template cache")
+			return nil, err
+		}
+
+		app.TemplateCache = tc
+	}
+
+	repo := handlers.NewRepo(&app, dbConn)
+	handlers.SetRepo(repo)
+	render.NewRenderer(&app)
 
 	return dbConn, nil
 }
